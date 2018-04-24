6ec8fefc7340fe95cc64884280e3ef70d08f3408
Sean Izak <sean.izak.decena@gmail.com>
2018-04-24 01:18:21 +0000

    Added game pause functionality

--- snake.js
+++ snake.js
@@ -23,6 +23,8 @@ References HTML canvas and 2D Graphics context
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
+//document.getElementById("score").value = score;
+
 
 setInterval (update, 1000/10); //Sets game frame rate
 
@@ -45,62 +47,66 @@ function setUp () {
 Generates game characters including snake and food
 */
 function update () {
-  snakeX += xvel;
-  snakeY += yvel;
-
-  if (snakeX > width) {  //wraps snake location if in contact with walls
-    snakeX = 0;
-  } else if (snakeX < 0) {
-    snakeX = width;
-  } else if (snakeY > height) {
-    snakeY = 0;
-  } else if (snakeY < 0) {
-    snakeY = height;
-  }
-
-  ctx.fillStyle = '#002b36'; //creates background
-  ctx.fillRect(0,0,width,height);
-
-  ctx.fillStyle = "#f4ce42"; //creates tail
-  for (var i = 0; i < trail.length; i++) { 
-      ctx.fillRect(trail[i].x, trail[i].y, cellSize, cellSize);
-      if (snakeX == trail[i].x && snakeY == trail[i].y) { //if snake head collides with tail
-        tail = 5;
-        score = 0;
-      }
-  }
-
-  ctx.fillStyle = "#FF0000"; //creates food
-  var fruit = ctx.fillRect(fruitX, fruitY, cellSize, cellSize);
-
-  ctx.fillStyle = "#f4ce42"; //creates snake head
-  var snake = ctx.fillRect(snakeX,snakeY,cellSize,cellSize);
-
-  ctx.fillStyle = ' #002b36 '; //score counter
-  ctx.fillText("Score: " + score, 350,375);
-
-  trail.push({x: snakeX, y: snakeY});
-  while (trail.length > tail) { //restrains tail to amount gained by eating fruit
-    trail.shift();
-  }
-
-  function checkEatsFruit () { //collision detection for fruit
-    if (snakeX < fruitX + cellSize &&
-       snakeX + cellSize > fruitX &&
-       snakeY < fruitY + cellSize &&
-       cellSize + snakeY > fruitY) {
-        score+=10;
-        tail++;
-        fruitX = Math.floor(Math.random() * cellSize)*cellSize;
-        fruitY = Math.floor(Math.random() * cellSize)*cellSize;
-        eatsFruit = true;
-
-    } else {
-      eatsFruit = false;
+  
+  if (!gameOver) {
+    snakeX += xvel;
+    snakeY += yvel;
+  
+    if (snakeX > width) {  //wraps snake location if in contact with walls
+      snakeX = 0;
+    } else if (snakeX < 0) {
+      snakeX = width;
+    } else if (snakeY > height) {
+      snakeY = 0;
+    } else if (snakeY < 0) {
+      snakeY = height;
     }
+  
+    ctx.fillStyle = '#002b36'; //creates background
+    ctx.fillRect(0,0,width,height);
+  
+    ctx.fillStyle = "#f4ce42"; //creates tail
+    for (var i = 0; i < trail.length; i++) { 
+        ctx.fillRect(trail[i].x, trail[i].y, cellSize, cellSize);
+        if (snakeX == trail[i].x && snakeY == trail[i].y) { //if snake head collides with tail
+          tail = 5;
+          score = 0;
+        }
+    }
+  
+    ctx.fillStyle = "#FF0000"; //creates food
+    var fruit = ctx.fillRect(fruitX, fruitY, cellSize, cellSize);
+  
+    ctx.fillStyle = "#f4ce42"; //creates snake head
+    var snake = ctx.fillRect(snakeX,snakeY,cellSize,cellSize);
+  
+    ctx.fillStyle = 'white'; //score counter
+    ctx.fillText("Score: " + score, 350,375);
+  
+  
+    trail.push({x: snakeX, y: snakeY});
+    while (trail.length > tail) { //restrains tail to amount gained by eating fruit
+      trail.shift();
+    }
+  
+    function checkEatsFruit () { //collision detection for fruit
+      if (snakeX < fruitX + cellSize &&
+         snakeX + cellSize > fruitX &&
+         snakeY < fruitY + cellSize &&
+         cellSize + snakeY > fruitY) {
+          score+=10;
+          tail++;
+          fruitX = Math.floor(Math.random() * cellSize)*cellSize;
+          fruitY = Math.floor(Math.random() * cellSize)*cellSize;
+          eatsFruit = true;
+      } else {
+        eatsFruit = false;
+      }
+    }
+    checkEatsFruit();
   }
-  checkEatsFruit();
 }
+
 /*
 Controls game movements
 */
@@ -110,18 +116,27 @@ function input () {
     if (key == 38) {
       xvel = 0;
       yvel = -20;
+      gameOver = false;
      }else if (key == 40) {
       xvel = 0;
       yvel = 20;
+      gameOver = false;
     } else if (key == 39) {
       yvel = 0;
       xvel = 20;
+      gameOver = false;
     } else if (key == 37) {
       yvel = 0;
       xvel = -20;
+      gameOver = false;
+    } else if (key == 80) {
+      xvel = 0;
+      yvel = 0;
+      gameOver = true;
     }
   }
 }
+
 
 //entry point
 setUp();
