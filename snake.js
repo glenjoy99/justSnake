303fb39a692e579d28c2a6beece206fc71355eae
Sean Izak <sean.izak.decena@gmail.com>
2018-04-26 16:29:49 +0000

    Added high score feature and added game over and pause screens

--- snake.js
+++ snake.js
@@ -5,7 +5,15 @@ Copyright 2018
 Decena Software Solutions
 */
 var score;
-var gameOver = false;
+var highscore = 0;
+var gameOver;
+var paused = false;
+
+var goingUp = false;
+var goingDown = false;
+var goingLeft = false;
+var goingRight = false;
+
 var eatsFruit;
 const width = 600;
 const height = 600;
@@ -17,7 +25,7 @@ var fruitY;
 var xvel;
 var yvel;
 var trail = [];
-var tail = 5;
+var tail = 0;
 /*
 References HTML canvas and 2D Graphics context
 */
@@ -25,7 +33,7 @@ var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
 
-setInterval (update, 1000/10); //Sets game frame rate
+setInterval (update, 1000/15); //Sets game frame rate
 
 /*
 Initializes score and character coordinates
@@ -40,7 +48,7 @@ function setUp () {
   snakeY = height/2;
   fruitX = Math.floor(Math.random() * cellSize)*cellSize;
   fruitY = Math.floor(Math.random() * cellSize)*cellSize;
-  tail = 5;
+  tail = 0;
 }
 /*
 Generates game characters including snake and food
@@ -52,39 +60,48 @@ function update () {
     snakeX += xvel;
     snakeY += yvel;
   
-    if (snakeX > width) {  //wraps snake location if in contact with walls
-      snakeX = 0;
+    if (snakeX > width) {  //game over if you collide with the walls
+      gameOver = true;
     } else if (snakeX < 0) {
-      snakeX = width;
+      gameOver = true;
     } else if (snakeY > height) {
-      snakeY = 0;
+      gameOver = true;
     } else if (snakeY < 0) {
-      snakeY = height;
+      gameOver = true;
     }
   
     ctx.fillStyle = '#002b36'; //creates background
     ctx.fillRect(0,0,width,height);
   
-    ctx.fillStyle = "#46aa29"; //creates tail
-    for (var i = 0; i < trail.length; i++) { 
-        ctx.fillRect(trail[i].x, trail[i].y, cellSize, cellSize);
-        if (snakeX == trail[i].x && snakeY == trail[i].y && tail > 5) { //if snake head collides with tail
-          score = 0;
-          tail = 5;
-          xvel = 0;
-          yvel = 0;
-          gameOver = true;
-          document.getElementById('gameOver').style.display='block';
-        }
+    if (!paused) {
+      ctx.fillStyle = "#46aa29"; //creates tail
+      for (var i = 0; i < trail.length; i++) { 
+          ctx.fillRect(trail[i].x, trail[i].y, cellSize, cellSize);
+          if (snakeX == trail[i].x && snakeY == trail[i].y && tail > 0 && paused == false) { //if snake head collides with tail
+            tail = 0;
+            xvel = 0;
+            yvel = 0;
+            gameOver = true;
+            document.getElementById('gameOver').style.display= 'block';
+            document.getElementById('scorePrint').innerHTML= "Score: " + score;
+          }
+      }
     }
+   
   
     ctx.fillStyle = "#898a8c"; //creates food
     var fruit = ctx.fillRect(fruitX, fruitY, cellSize, cellSize);
   
     ctx.fillStyle = "#46aa29"; //creates snake head
     var snake = ctx.fillRect(snakeX,snakeY,cellSize,cellSize);
-  
-    document.getElementById('scoreNum').innerHTML = "Score: " + score; //score counter
+    
+    if (score > highscore){
+      highscore = score;
+    }
+    
+    //score counter  
+    document.getElementById('scoreNum').innerHTML = "Score: " + score; 
+    document.getElementById('highscoreNum').innerHTML = "Highscore: " + highscore; 
     
     trail.push({x: snakeX, y: snakeY});
     while (trail.length > tail) { //restrains tail to amount gained by eating fruit
@@ -96,8 +113,8 @@ function update () {
          snakeX + cellSize > fruitX &&
          snakeY < fruitY + cellSize &&
          cellSize + snakeY > fruitY) {
-          score+=10;
-          tail++;
+          score+=5;
+          tail += 5;
           fruitX = Math.floor(Math.random() * cellSize)*cellSize;
           fruitY = Math.floor(Math.random() * cellSize)*cellSize;
           eatsFruit = true;
@@ -106,10 +123,11 @@ function update () {
       }
     }
     checkEatsFruit();
+  } else {
+    document.getElementById('gameOver').style.display= 'block';
+    document.getElementById('scorePrint').innerHTML= "Score: " + score;
+    document.getElementById('highscorePrint').innerHTML= "Highscore: " + highscore;
   }
-  
-  
-  
 }
 
 
@@ -118,34 +136,72 @@ function update () {
 Controls game movements
 */
 function input () {
-  window.onkeyup = function(e) {
+  window.onkeydown = function(e) {
     var key = e.keyCode ? e.keyCode : e.which;
-    if (key == 38) {
+    if (key == 38 && gameOver == false && goingDown == false) { //up
+      goingUp = true;
+      goingRight = false;
+      goingLeft = false;
+      goingDown == false
       xvel = 0;
       yvel = -20;
       gameOver = false;
-      document.getElementById('gameState').innerHTML = "";
-     }else if (key == 40) {
+      paused = false;
+      document.getElementById('pause').style.display='none';
+     }else if (key == 40 && gameOver == false && goingUp == false) { //down
+      goingDown = true;
+      goingRight = false;
+      goingLeft = false;
+      goingUp == false
       xvel = 0;
       yvel = 20;
       gameOver = false;
-      document.getElementById('gameState').innerHTML = "";
-    } else if (key == 39) {
+      paused = false;
+      document.getElementById('pause').style.display='none';
+    } else if (key == 39 && gameOver == false && goingLeft == false) { //right
+      goingRight = true;
+      goingUp = false;
+      goingDown = false;
+      goingLeft == false
       yvel = 0;
       xvel = 20;
       gameOver = false;
-      document.getElementById('gameState').innerHTML = "";
-    } else if (key == 37) {
+      paused = false;
+      document.getElementById('pause').style.display='none';
+    } else if (key == 37 && gameOver == false  && goingRight == false) { //left
+      goingLeft = true;
+      goingUp = false;
+      goingDown = false;
+      goingRight == false
       yvel = 0;
       xvel = -20;
       gameOver = false;
-      document.getElementById('gameState').innerHTML = "";
-    } else if (key == 80) {
-      xvel = 0;
+      paused = false;
+      document.getElementById('pause').style.display='none';
+    } else if (key == 80 && gameOver == false) { //pause
       yvel = 0;
-      gameOver = true;
-      document.getElementById('gameState').innerHTML = "GAME PAUSED";
+      xvel = 0;
+      paused = true;
+      
+      document.getElementById('pause').style.display='block';
     }
+    
+  }
+}
+
+//prevents the window from scrolling while using the arrow keys
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
+function reset () {
+  if(gameOver){
+    setUp();
+    update();
+    document.getElementById('gameOver').style.display='none';
   }
 }
 
@@ -154,3 +210,4 @@ setUp();
 input();
 update();
 
+  
